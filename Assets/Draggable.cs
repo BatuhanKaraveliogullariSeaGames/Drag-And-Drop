e13db7ba79e3e2411f3838fdff4e71136698422d
BatuhanKaraveliogullariSeaGames <77837676+BatuhanKaraveliogullariSeaGames@users.noreply.github.com>
2021-02-01 14:49:40 +0300

    Drag And Drop Tash has complated without UI

---
+++ Assets/Draggable.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    private Vector3 initialPosition;
+    [SerializeField] private Canvas canvas;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        initialPosition = rectTransform.anchoredPosition;
+        canvasGroup.alpha = .6f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+        if (!GetHitCondition(eventData))
+            rectTransform.anchoredPosition = initialPosition;
+    }
+
+    private bool GetHitCondition(PointerEventData eventData)
+    {
+        var results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+        foreach (var result in results)
+        {
+            if (result.gameObject.CompareTag("Slot"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
